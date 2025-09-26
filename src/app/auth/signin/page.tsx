8450ff00eb398e971cd056bddddc98e32959e3ca
ardianilyas <ardianilyas@gmail.com>
2025-09-27 01:46:58 +0700

    feat: add sign in and sign up page

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+import SignInForm from '@/features/auth/components/SigninForm'
+import React from 'react'
+
+export default function Signin() {
+  return (
+    <div>
+        <SignInForm />
+    </div>
+  )
+}
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,11 @@
+
+import SignUpForm from '@/features/auth/components/SignupForm'
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+        <SignUpForm />
+    </div>
+  )
+}
